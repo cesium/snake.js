460171f9e15b8064af29b13315a9feef744cdd47
Miguel Brandão <mickael.a.brandao@gmail.com>
2018-12-04 17:50:51 +0000

    Snake cresce e movimenta-se com efeitos

--- snake.js
+++ snake.js
@@ -8,22 +8,33 @@ class Snake {
   }
   
   update(){
-  	this.body[0].x += this.xdir;
-    this.body[0].y += this.ydir;
+  	let head = this.body[0].copy();
+    this.body.pop();
+    head.x += this.xdir;
+    head.y += this.ydir;
+    this.body.unshift(head);
   }
   
   show(){
   	fill(0);
-    rect(this.body[0].x, this.body[0].y, 1, 1);
+    for (let i = 0; i < this.body.length; i++){
+    	rect(this.body[i].x, this.body[i].y, 1, 1);
+    }
   }
 
   eat(food){
   	let head = this.body[0];
     if (head.x == food.x && head.y == food.y){
     	print("FOOD EATEN");
+      this.grow();
       return true;
     }
     return false;
+  }
+  
+  grow(){
+  	let tail = this.body[this.body.length-1].copy();
+    this.body.push(tail);
   }
   
 }
